67f213d493475994f516129ec65780871bf0af96
John Nielsen <johnhayesnielsen@gmail.com>
2020-06-23 00:25:13 -0600

    created html through node. Also created modules.

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+const [name, github] = profileDataArgs;
+    
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i<profileDataArr.length; i +=1) {
-    console.log(profileDataArr[i]);
+fs.writeFile('index.html', generatePage(name, github,), err => {
+    if (err) throw err;
 
-    console.log('================');
+    console.log('Portfolio complete! Check out the index.html to see the output!');
+});
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    }
-};
+// JUST FOR REFERENCE PURPOSES
 
-printProfileData(profileDataArgs)
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs)
+
+// const printProfileData = profileDataArr => {
+//     for (let i=0; i<profileDataArr.length; i +=1) {
+//     console.log(profileDataArr[i]);
+
+//     console.log('================');
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//     }
+// };
+
+// printProfileData(profileDataArgs)
