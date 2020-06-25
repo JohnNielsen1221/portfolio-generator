331c9fd57ffff248af10de7f1f0b69a02c1d6c8b
John Nielsen <johnhayesnielsen@gmail.com>
2020-06-25 00:01:19 -0600

    finished setting up html layout

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template.js');
+
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -125,17 +128,22 @@ const promptProject = portfolioData => {
         }
     })
 }
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+          console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
 
 
+    
 
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const [name, github] = profileDataArgs;
 
